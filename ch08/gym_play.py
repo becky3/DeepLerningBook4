1fef845ea33a885a3111dbcd6871143dda2ff26b
becky <satoru-kibe@rhythmcan.com>
2024-05-24 09:25:40 +0900

    8章 その1

---
+++ ch08/gym_play.py
@@ -0,0 +1,14 @@
+﻿import numpy as np
+import gym
+
+env = gym.make("CartPole-v1", render_mode="human")
+state = env.reset()
+terminated = False
+
+while not terminated:
+    env.render()
+    action = np.random.choice([0, 1])
+    next_state, reward, terminated, truncated, info = env.step(action)
+    print(f"{action}, {terminated}")
+
+env.close()
